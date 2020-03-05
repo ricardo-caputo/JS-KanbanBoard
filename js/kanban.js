b7410756133dfa83828cb24c478b6523b5416688
Ricardo Caputo <ricardo.caputo@outlook.com>
2020-03-05 15:37:14 -0500

    'module1'

--- js/kanban.js
+++ js/kanban.js
@@ -46,10 +46,11 @@ const create_item = () => {
 
 document.querySelectorAll('.drop').forEach(element => {
   element.addEventListener("drop", (event) => {
-    let id = event.dataTransfer.getData('text');
+    event.preventDefault();
+    const id = event.dataTransfer.getData('text');
     event.target.appendChild(document.getElementById('id'));
   });
   element.addEventListener("dragover", (event) => {
     event.preventDefault();
-  })
+  });
 });
